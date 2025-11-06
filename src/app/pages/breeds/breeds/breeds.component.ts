b5782e4ecf07727cb7cfcb4116e2eeefbe2d4686
Daniel Abrahao <daniel@preface.com.br>
2025-11-06 01:32:44 -0300

    Adiciona busca

--- src/app/pages/breeds/breeds/breeds.component.ts
+++ src/app/pages/breeds/breeds/breeds.component.ts
@@ -1,45 +1,69 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {DogApiService} from '../../../core/services/dog-api.service';
 import {Breed} from '../../../core/models/breed.model';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
 import {RouterLink} from '@angular/router';
+import {NgClass} from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {debounceTime, distinctUntilChanged, filter, Observable, switchMap, tap} from 'rxjs';
+import {PaginatedResponse} from '../../../core/models/paginated-response.model';
 
 @Component({
   selector: 'app-breeds',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, RouterLink],
+  imports: [MatTableModule, MatPaginatorModule, RouterLink, NgClass, MatFormFieldModule, MatIconModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './breeds.component.html',
   styleUrl: './breeds.component.scss'
 })
 export class BreedsComponent implements OnInit {
   private readonly dogApiService = inject(DogApiService);
+  pageSize = signal(5);
+  pageIndex = signal(0);
+  length = signal(0);
+  loading = signal(true);
+
+  searchControl = new FormControl('');
+
   displayedColumns: string[] = ['name', 'bred_for', 'breed_group', 'life_span', 'temperament'];
   dataSource = new MatTableDataSource<Breed>();
-  pageSize: number = 5;
-  pageIndex: number = 0;
-  length?: number;
-  loading = true;
 
   ngOnInit() {
     this.updateList();
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(term => {
+      this.updateList();
+    });
   }
 
   handlePageEvent(e: PageEvent) {
-    this.length = e.length;
-    this.pageSize = e.pageSize;
-    this.pageIndex = e.pageIndex;
+    this.length.set(e.length);
+    this.pageSize.set(e.pageSize);
+    this.pageIndex.set(e.pageIndex);
     this.updateList();
   }
 
   private updateList() {
-    this.loading = true;
-    this.dogApiService.getBreeds(this.pageSize, this.pageIndex).subscribe(paginatedBreeds => {
-      this.length = paginatedBreeds.pagination.count;
-      this.pageSize = paginatedBreeds.pagination.limit;
-      this.pageIndex = paginatedBreeds.pagination.page;
+    this.loading.set(true);
+
+    let observable: Observable<PaginatedResponse<Breed[]>>;
+    if (this.searchControl.value?.trim()) {
+      observable = this.dogApiService.searchBreeds(this.searchControl.value!, this.pageSize(), this.pageIndex())
+    } else {
+      observable = this.dogApiService.getBreeds(this.pageSize(), this.pageIndex());
+    }
+
+    observable.subscribe(paginatedBreeds => {
+      this.length.set(paginatedBreeds.pagination.count);
+      this.pageSize.set(paginatedBreeds.pagination.limit);
+      this.pageIndex.set(paginatedBreeds.pagination.page);
       this.dataSource.data = paginatedBreeds.data;
-      this.loading = false;
+      this.loading.set(false);
     });
   }
 }
